d016e4477c8e9b5c955e1c857a3973957b4f52ec
David Turissini <davidturissini@Melissas-Air.home>
2025-01-08 11:12:44 -0800

    command is running

--- packages/remove-unused/src/main.ts
+++ packages/remove-unused/src/main.ts
@@ -3,18 +3,22 @@ import { analyze } from '@remove-unused/engine';
 
 main()
 
-export function main() {
-  yargs()
-    .command('$0 [cwd]', 'Remove unused files from your project', async (yargs) => {
-      return yargs.positional('cwd', {
-        describe: 'Directory to scan',
-        default: process.cwd(),
-      });
-    }, async ({ cwd }: { cwd: string }) => {
-      const results = await analyze({
-        cwd,
-      });
-      console.log(results);
-    })
-    .help().argv
+export async function main() {
+  yargs(process.argv.slice(2))
+    .command(
+      '$0 [cwd]',
+      'Remove unused files from your project',
+      // @ts-expect-error
+      async (yargs) => {
+        return yargs.positional('cwd', {
+          describe: 'Directory to scan',
+          default: process.cwd(),
+        });
+      }, async ({ cwd }: { cwd: string }) => {
+        const results = await analyze({
+          cwd,
+        });
+        process.stdout.write(JSON.stringify(results, null, 2));
+      })
+    .help().argv;
 }
