d2b8f6dff190665ae7805343d32b82bb7af76a37
David Turissini <davidhturissini@gmail.com>
2025-01-25 16:59:50 -0800

    fixing eslint mistake

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -16,5 +16,4 @@ module.exports = [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   ...eslintPluginAstro.configs['flat/recommended'],
-  ...eslintPluginAstro.configs['jsx-a11y-strict'],
 ];
